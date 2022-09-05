e8cfb1015a435df234228614b0048a3b55921071
Adryel Simon <adryel@live.it>
2022-09-05 05:32:26 -0300

    chore: innerhtml and title name

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ element.innerHTML = `<!doctype html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Need a real server for send</title>
 </head>
 
 <body class="d-flex flex-column">
